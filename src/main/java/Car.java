368297ef6ef328d91acf6a0c977abc27f4d924bc
SErdmannWI <scott.erdmann@mykenzie.snhu.edu>
2023-06-15 10:33:13 -0500

    Adding code to car object

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,35 @@
 public class Car {
+    private String carName;
+    private double carPrice;
+    private boolean isElectric;
+
+    public Car(String name, double price, boolean isElectric) {
+        this.carName = name;
+        this.carPrice = price;
+        this.isElectric = isElectric;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public double getCarPrice() {
+        return carPrice;
+    }
+
+    public void setCarPrice(double carPrice) {
+        this.carPrice = carPrice;
+    }
+
+    public boolean isElectric() {
+        return isElectric;
+    }
+
+    public void setElectric(boolean electric) {
+        isElectric = electric;
+    }
 }
